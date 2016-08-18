c9a66510cf8185549e0af830d868306c2b883161
Megan Coyle <meg@mcoyle.com>
2016-08-18 14:03:58 -0400

    Add redirect for after updating song

--- public/js/songs/show.controller.js
+++ public/js/songs/show.controller.js
@@ -6,17 +6,20 @@
   .controller("SongShowController", [
     "$stateParams",
     "$firebaseObject",
+    "$state",
     SongShowControllerFunction
   ]);
 
-  function SongShowControllerFunction($stateParams, $firebaseObject) {
+  function SongShowControllerFunction($stateParams, $firebaseObject, $state) {
     var vm = this;
     var ref = firebase.database().ref().child("songs/" + $stateParams.id);
     $firebaseObject(ref).$loaded().then(function(song){
       vm.song = song
     });
     vm.update = function(){
-      vm.song.$save();
+      vm.song.$save().then(function() {
+        $state.go("songIndex");
+      })
     }
   }
 
